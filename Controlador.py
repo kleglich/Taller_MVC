192570960d99124625dd976d1bc907a4f235c18b
Klei <klei6432@gmail.com>
2026-05-09 12:30:30 -0500

    Update Controlador.py

--- Controlador.py
+++ Controlador.py
@@ -4,15 +4,22 @@ from Vista_fomulario import Vista_formulario
 class Controlador:
     def __init__(self):
         self.obj_numero = Numero()
-        self.obj_vista = Vista_formulario()
+        self.obj_vista = Vista_formulario(self)
 
     def tomar_numero(self):
-        valor = self.obj_vista.pedir_numero()
-        self.obj_numero.set_numero(valor)
-        self.obj_vista.imprimir_mensaje("Número recibido.")
+        try:
+            valor = self.obj_vista.pedir_numero()
+            self.obj_numero.set_numero(valor)
+            self.obj_vista.imprimir_mensaje("Numero recibido correctamente.")
+            self.imprimir_numero()
+            
+        except ValueError:
+            self.obj_vista.imprimir_mensaje("Error: Debe ingresar un número válido.")
 
-    def procesar(self):
-        numero_actual = self.obj_numero.get_numero()
-        paridad = self.obj_numero.validar_paridad()
-        naturaleza = self.obj_numero.validar_naturaleza()
-        self.obj_vista.imprimir_resultado(numero_actual, paridad, naturaleza)
+    def imprimir_numero(self):
+        num = self.obj_numero.get_numero()
+        resultado = self.obj_numero.validar_numero(num)
+        self.obj_vista.imprimir_numero(resultado)
+
+    def iniciar_app(self):
+        self.obj_vista.iniciar()
