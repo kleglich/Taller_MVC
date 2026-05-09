38e75c3ca0ad6395056c1d5588797686ff84d638
Klei <klei6432@gmail.com>
2026-05-09 12:31:29 -0500

    Update Numero_modelo.py

--- Numero_modelo.py
+++ Numero_modelo.py
@@ -1,4 +1,3 @@
-
 class Numero:
     def __init__(self):
         self.dato_numero = 0
@@ -9,15 +8,9 @@ class Numero:
     def get_numero(self):
         return self.dato_numero
 
-    def validar_paridad(self):
-        if self.dato_numero % 2 == 0:
-            return "Par"
-        return "Impar"
-
-    def validar_naturaleza(self):
-        if self.dato_numero > 0:
-            return "Positivo"
-        elif self.dato_numero < 0:
-            return "Negativo"
-        else:
-            return "Neutro"
+    def validar_numero(self, dato_numero):
+        paridad = "Par" if dato_numero % 2 == 0 else "Impar"
+        if dato_numero > 0: nat = "Positivo"
+        elif dato_numero < 0: nat = "Negativo"
+        else: nat = "Neutro"
+        return f"{paridad} y {nat}"
